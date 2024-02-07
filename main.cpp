d31bb42308f1c276589bf3f75e90be87026cb7ea
lunatics975 <116869422+lunatics975@users.noreply.github.com>
2024-02-07 22:15:13 +0000

    add data types; variable definition; output

--- main.cpp
+++ main.cpp
@@ -1,25 +1,38 @@
 //Example for C++ Encapsulation
 #include <iostream>
 #include <string>
-
+/*creates class bank*/
 class bank{
     private:
     int value = 3000;
     public:
+    double lost = 3.76;
+    char Letter = 'A';
+    bool Boolean = true;
+    float Float = 3.14e10;
     int getnumber();
     void setnumber(int wert);
+    void getboolean(){
+        std::cout<<"china: "<<Boolean<<"\n";
+    }
 };
 
 int main(){
+    const bool china = true;
     int lol;
+    int a, b=10, c;
+    a = b = 10;
     std::string accountname;
     std::cout<<"Hello, type in your number \n";
     std::cin>>lol;
     std::cout<<"Also type in your name"<<std::endl;
+    std::cout<<"Hi\\lollol\"lostx3\"haha\tlost"<<std::endl;
     getline(std::cin, accountname);
+    //new object myaccount
     bank mya;
     mya.setnumber(lol);
     std::cout<<"Your number is: "<<mya.getnumber()<<std::endl;
+    mya.getboolean();
     return 0;
 }
 
